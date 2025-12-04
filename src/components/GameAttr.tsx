f46b917c2be14dfc685ab7d1251175bec5649d3b
Karam Ghanem <karamghanem34@gmail.com>
2025-12-04 03:51:49 +0300

    updatae resposive

--- src/components/GameAttr.tsx
+++ src/components/GameAttr.tsx
@@ -1,6 +1,14 @@
-import { Badge, HStack, Stack, Box, Text } from "@chakra-ui/react";
+import {
+  Badge,
+  HStack,
+  Stack,
+  Box,
+  Text,
+  SimpleGrid,
+  GridItem,
+} from "@chakra-ui/react";
 import GameAttrributes from "@/components/GameAttrributes";
-import type Game  from "@/entities/Game";
+import type Game from "@/entities/Game";
 
 interface Props {
   game?: Game;
@@ -9,46 +17,61 @@ interface Props {
 const GameAttr = ({ game }: Props) => {
   return (
     <>
-      <HStack marginBottom={10} marginTop={30}>
-        <Box bg="" marginRight={40}>
-          <GameAttrributes term="Platforms">
-            {game?.platforms?.map(({ platform }) => (
-              <Text key={platform.id}>{platform.name}</Text>
-            ))}
-          </GameAttrributes>
-        </Box>
+      <SimpleGrid columns={{ base: 1, sm: 2, lg: 2, xl: 2 }} marginBottom={10} marginTop={30}>
+        <GridItem marginBottom={10}>
+          {" "}
+          <Box bg="" marginRight={40}>
+            <GameAttrributes term="Platforms">
+              {game?.platforms?.map(({ platform }) => (
+                <Text key={platform.id}>{platform.name}</Text>
+              ))}
+            </GameAttrributes>
+          </Box>
+        </GridItem>
+        <GridItem marginBottom={10}>
+          {" "}
+          <Box bg="" marginLeft="-200px">
+            <GameAttrributes term="Genres">
+              {game?.genres.map((genre) => (
+                <Text key={genre.id}>{genre.name}</Text>
+              ))}
+            </GameAttrributes>
+          </Box>
+        </GridItem>
+        <GridItem marginBottom={10} >
+          {" "}
+          <Box bg="" marginRight={40}>
+            <GameAttrributes term="Metascore">
+              <Stack direction="row">
+                <Badge
+                  colorPalette="green"
+                  padding={2}
+                  fontSize={15}
+                  fontWeight="bold"
+                >
+                  {game?.metacritic}
+                </Badge>
+              </Stack>
+            </GameAttrributes>
+          </Box>
+        </GridItem>
+        <GridItem marginBottom={10}>
+          {" "}
+          <Box bg="">
+            <GameAttrributes term="WebSite">
+              <Text>{game?.website}</Text>
+            </GameAttrributes>
+          </Box>
+        </GridItem>
+      </SimpleGrid>
 
-        <Box bg="" marginTop="-98px">
-          <GameAttrributes term="Genres">
-            {game?.genres.map((genre) => (
-              <Text key={genre.id}>{genre.name}</Text>
-            ))}
-          </GameAttrributes>
-        </Box>
-      </HStack>
+      {/* <HStack marginBottom={10} marginTop={30}> */}
 
-      <HStack>
-        <Box bg="" marginRight={40}>
-          <GameAttrributes term="Metascore">
-            <Stack direction="row">
-              <Badge
-                colorPalette="green"
-                padding={2}
-                fontSize={15}
-                fontWeight="bold"
-              >
-                {game?.metacritic}
-              </Badge>
-            </Stack>
-          </GameAttrributes>
-        </Box>
+      {/* </HStack> */}
 
-        <Box bg="">
-          <GameAttrributes term="WebSite">
-            <Text>{game?.website}</Text>
-          </GameAttrributes>
-        </Box>
-      </HStack>
+      {/* <HStack> */}
+
+      {/* </HStack> */}
     </>
   );
 };
